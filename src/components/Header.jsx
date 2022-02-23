322109d1eac8fc2381d68627766212c4376c1013
manuelvalderrama <mvalderrama@falabella.com>
2022-02-23 14:24:20 -0500

    change to sidebar, add content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,50 +1,30 @@
 import React from "react";
 const navigation = [
-  { name: "Produccion", href: "#" },
-  { name: "Calidad", href: "#" },
-  { name: "Issues", href: "#" },
+  { name: "Usuario", href: "#" },
+  { name: "Actividad", href: "#" },
+  { name: "Reportar un problema", href: "#" },
 ];
 
 export default function Header(props) {
   return (
-    <header className="bg-indigo-600 fixed left-0 right-0 top-0">
-      <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
-        <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
-          <div className="flex items-center">
-            <a href="#">
-              <span className="sr-only">Workflow</span>
-            </a>
-            <div className="hidden ml-10 space-x-8 lg:block">
-              {navigation.map((link) => (
-                <a
-                  key={link.name}
-                  href={link.href}
-                  className="text-base font-medium text-white hover:text-indigo-50"
-                >
-                  {link.name}
-                </a>
-              ))}
-            </div>
-            <button onClick={props.logout} className="text-white">
-              Log out
-            </button>
-          </div>
-        </div>
-        <div className="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
-          {navigation.map((link) => (
-            <a
-              key={link.name}
-              href={link.href}
-              className="text-base font-medium text-white hover:text-indigo-50"
-            >
-              {link.name}
-            </a>
-          ))}
-          <button onClick={props.logout} className="text-white">
-            Log out
-          </button>
-        </div>
-      </nav>
+    <header className="bg-yellow-500 z-10 fixed left-0 right-0 top-0">
+      <div className="flex items-center h-16 ml-10 w-full space-x-8">
+        {navigation.map((link) => (
+          <a
+            key={link.name}
+            href={link.href}
+            className="text-base font-medium text-white hover:text-indigo-50"
+          >
+            {link.name}
+          </a>
+        ))}
+        <button
+          onClick={props.logout}
+          className="m-auto text-base font-medium text-white hover:text-indigo-50 mx-4 rounded-md bg-gray-500 p-2 my-0"
+        >
+          Log out
+        </button>
+      </div>
     </header>
   );
 }
